12844d61064d00f5e68d5e8044f15b8ccb5e1a13
melvinmng <melvinmng@icloud.com>
2025-06-06 09:47:53 +0200

    Add responsive navbar and update tests

--- world-app/src/app/app.component.spec.ts
+++ world-app/src/app/app.component.spec.ts
@@ -14,16 +14,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'world-app' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('world-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, world-app');
-  });
+  // Additional tests can be added here as the application grows
 });
