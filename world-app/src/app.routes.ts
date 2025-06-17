5ee1637682f7cdc799684477df5670720a2f2c42
melvinmng <melvinmng@icloud.com>
2025-06-17 20:25:30 +0200

    added account management component

--- world-app/src/app.routes.ts
+++ world-app/src/app.routes.ts
@@ -3,11 +3,13 @@ import { LoginComponent } from './app/pages/login/login.component';
 import { RegisterComponent } from './app/pages/register/register.component';
 import { MapComponent } from './app/pages/map/map.component';
 import { FlightSearchComponent } from './app/pages/flight-search/flight-search.component';
+import { AccountManagementComponent } from './app/pages/account-management/account-management.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'map', component: MapComponent },
   { path: 'flight-search', component: FlightSearchComponent },
+  { path: 'account-management', component: AccountManagementComponent },
   { path: '**', redirectTo: '' }
 ];
---
+++ world-app/src/app/pages/account-management/account-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-account-management',
+  imports: [],
+  templateUrl: './account-management.component.html',
+  styleUrl: './account-management.component.css'
+})
+export class AccountManagementComponent {
+
+}
