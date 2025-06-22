20c9e83b8d8961ff1e9cc179525324a3fe64b4ba
RickyNguyen04 <ricky04nguyen@gmail.com>
2025-06-22 02:56:48 +0200

    Improve dashboard management

--- world-app/src/app/pages/dashboard-management/dashboard-management.component.ts
+++ world-app/src/app/pages/dashboard-management/dashboard-management.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 import { NgFor } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [NgFor],
-  templateUrl: './dashboard-management.component.html'
+  imports: [NgFor, NavbarComponent],
+  templateUrl: './dashboard-management.component.html',
+  styleUrls: ['./dashboard-management.component.css']
 })
 export class DashboardComponent {
   services = [
-    { name: 'API-Service', path: '../api-service/app.py', port: 5000 },
-    { name: 'Auth-Service', path: '../api-service/app.py', port: 5002 },
-    { name: 'Flight-Service', path: '../api-service/app.py', port: 5003 },
-    { name: 'DB-Service', path: '../api-service/app.py', port: 5004 },
+    { name: 'API-Service', path: '../api-service/app.py', port: 5001 },
+    { name: 'Auth-Service', path: '../auth-service/app.py', port: 5002 },
+    { name: 'Flight-Service', path: '../flight-service/app.py', port: 5003 },
+    { name: 'DB-Service', path: '../db-service/app.py', port: 5004 },
   ];
 
   openService(service: any) {
